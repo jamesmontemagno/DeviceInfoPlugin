6376cbb26b3e7c752271d011675387eada54a40a
James Montemagno <james.montemagno@gmail.com>
2017-12-22 10:58:52 -0800

    Update mac implementation

--- src/DeviceInfo.Plugin.iOS/DeviceInfoImplementation.cs
+++ src/DeviceInfo.Plugin.iOS/DeviceInfoImplementation.cs
@@ -42,11 +42,11 @@ using System;
 
 namespace Plugin.DeviceInfo
 {
-    /// <summary>
-    /// Implementation for DeviceInfo
-    /// </summary>
-    public class DeviceInfoImplementation : IDeviceInfo
-    {
+	/// <summary>
+	/// Implementation for DeviceInfo
+	/// </summary>
+	public class DeviceInfoImplementation : IDeviceInfo
+	{
 
 		/// <summary>
 		/// Get the name of the device
@@ -65,137 +65,145 @@ namespace Plugin.DeviceInfo
 #elif __WATCHOS__
 				return WKInterfaceDevice.CurrentDevice.Name;
 #elif __MACOS__
-				return Environment.MachineName;
+				using (var name = ObjCRuntime.Runtime.GetNSObject<NSString>(SCDynamicStoreCopyComputerName(IntPtr.Zero, IntPtr.Zero), true))
+					return name;
 #endif
 			}
 		}
 
 #if __MACOS__
-        NSProcessInfo info;
-        string id, model = null;
+		NSProcessInfo info;
+		string id, model = null;
 #endif
 		/// <summary>
 		/// Default Constructor
 		/// </summary>
 		public DeviceInfoImplementation()
-        {
+		{
 
 #if __MACOS__
-            info = new NSProcessInfo();
+			info = new NSProcessInfo();
 #endif
-        }
-        /// <inheritdoc/>
-        public string GenerateAppId(bool usingPhoneId = false, string prefix = null, string suffix = null)
-        {
-            var appId = "";
+		}
+		/// <inheritdoc/>
+		public string GenerateAppId(bool usingPhoneId = false, string prefix = null, string suffix = null)
+		{
+			var appId = "";
 
-            if (!string.IsNullOrEmpty(prefix))
-                appId += prefix;
+			if (!string.IsNullOrEmpty(prefix))
+				appId += prefix;
 
-            appId += Guid.NewGuid().ToString();
+			appId += Guid.NewGuid().ToString();
 
-            if (usingPhoneId)
-                appId += Id;
+			if (usingPhoneId)
+				appId += Id;
 
-            if (!string.IsNullOrEmpty(suffix))
-                appId += suffix;
+			if (!string.IsNullOrEmpty(suffix))
+				appId += suffix;
 
-            return appId;
-        }
+			return appId;
+		}
 
-        /// <summary>
-        /// Returns the unique identifier of the device if supported
-        /// </summary>
+
+
+
+		/// <summary>
+		/// Returns the unique identifier of the device if supported
+		/// </summary>
 #if __MACOS__
-        public string Id => id ?? (id = GetSerialNumber());
+
+		[DllImport("/System/Library/Frameworks/SystemConfiguration.framework/SystemConfiguration")]
+		static extern IntPtr SCDynamicStoreCopyComputerName(IntPtr store, IntPtr encoding);
+
+		public string Id => id ?? (id = GetSerialNumber());
 
 
-        [DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
-        static extern uint IOServiceGetMatchingService(uint masterPort, IntPtr matching);
+		[DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
+		static extern uint IOServiceGetMatchingService(uint masterPort, IntPtr matching);
 
-        [DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
-        static extern IntPtr IOServiceMatching(string s);
+		[DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
+		static extern IntPtr IOServiceMatching(string s);
 
-        [DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
-        static extern IntPtr IORegistryEntryCreateCFProperty(uint entry, IntPtr key, IntPtr allocator, uint options);
+		[DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
+		static extern IntPtr IORegistryEntryCreateCFProperty(uint entry, IntPtr key, IntPtr allocator, uint options);
 
-        [DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
-        static extern int IOObjectRelease(uint o);
+		[DllImport("/System/Library/Frameworks/IOKit.framework/IOKit")]
+		static extern int IOObjectRelease(uint o);
 
-        string GetSerialNumber()
-        {
-            var serial = string.Empty;
+		string GetSerialNumber()
+		{
+			var serial = string.Empty;
 
-            try
-            {
-                
-                var platformExpert = IOServiceGetMatchingService(0, IOServiceMatching("IOPlatformExpertDevice"));
-                if (platformExpert != 0)
-                {
-                    var key = (NSString)"IOPlatformSerialNumber";
-                    var serialNumber = IORegistryEntryCreateCFProperty(platformExpert, key.Handle, IntPtr.Zero, 0);
-                    if (serialNumber != IntPtr.Zero)
-                    {
-                        serial = Runtime.GetNSObject<NSString>(serialNumber);
-                    }
-                    IOObjectRelease(platformExpert);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Unable to get serial number: " + ex.Message);
-            }
+			try
+			{
 
-            return serial;
-        }
+				var platformExpert = IOServiceGetMatchingService(0, IOServiceMatching("IOPlatformExpertDevice"));
+				if (platformExpert != 0)
+				{
+					var key = (NSString)"IOPlatformSerialNumber";
+					var serialNumber = IORegistryEntryCreateCFProperty(platformExpert, key.Handle, IntPtr.Zero, 0);
+					if (serialNumber != IntPtr.Zero)
+					{
+						serial = Runtime.GetNSObject<NSString>(serialNumber);
+					}
+					IOObjectRelease(platformExpert);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Unable to get serial number: " + ex.Message);
+			}
+
+			return serial;
+		}
 #elif __WATCHOS__
         public string Id => string.Empty;
 #else
         public string Id => UIDevice.CurrentDevice.IdentifierForVendor.AsString();
 #endif
 
-        /// <summary>
-        /// Returns the model of the device
-        /// </summary>
+		/// <summary>
+		/// Returns the model of the device
+		/// </summary>
 #if __MACOS__
-        public string Model => model ?? (model = GetModel());
+		public string Model => model ?? (model = GetModel());
 
-        string GetModel()
-        {
-            var modelString = string.Empty;
+		string GetModel()
+		{
+			var modelString = string.Empty;
 
-            try
-            {
-                var platformExpert = IOServiceGetMatchingService(0, IOServiceMatching("IOPlatformExpertDevice"));
-                if (platformExpert != 0)
-                {
-                    var modelKey = (NSString)"model";
-                    var model = IORegistryEntryCreateCFProperty(platformExpert, modelKey.Handle, IntPtr.Zero, 0);
-                    if (model != IntPtr.Zero)
-                    {
-                        modelString = Runtime.GetNSObject<NSString>(model);          
-                    }
-                    IOObjectRelease(platformExpert);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Unable to get model: " + ex.Message);
-            }
+			try
+			{
+				var platformExpert = IOServiceGetMatchingService(0, IOServiceMatching("IOPlatformExpertDevice"));
+				if (platformExpert != 0)
+				{
+					var modelKey = (NSString)"model";
+					var model = IORegistryEntryCreateCFProperty(platformExpert, modelKey.Handle, IntPtr.Zero, 0);
+					if (model != IntPtr.Zero)
+					{
+						modelString = Runtime.GetNSObject<NSString>(model);
+					}
+					IOObjectRelease(platformExpert);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Unable to get model: " + ex.Message);
+			}
 
-            return modelString;
-        }
+			return modelString;
+		}
 #elif __WATCHOS__
         public string Model => WKInterfaceDevice.CurrentDevice.Model;
 #else
         public string Model => UIDevice.CurrentDevice.Model;
 #endif
 
-        /// <summary>
-        /// Returns the version number as a string
-        /// </summary>
+		/// <summary>
+		/// Returns the version number as a string
+		/// </summary>
 #if __MACOS__
-        public string Version => info.OperatingSystemVersionString;
+		public string Version => info.OperatingSystemVersionString;
 #elif __WATCHOS__
         public string Version => WKInterfaceDevice.CurrentDevice.SystemVersion;
 #else
@@ -203,21 +211,21 @@ namespace Plugin.DeviceInfo
 #endif
 
 
-        /// <inheritdoc/>
-        public Version VersionNumber
-        {
-            get
-            {
-                try
-                {
-                    return new Version(Version);
-                }
-                catch
-                {
-                    return new Version();
-                }
-            }
-        }
+		/// <inheritdoc/>
+		public Version VersionNumber
+		{
+			get
+			{
+				try
+				{
+					return new Version(Version);
+				}
+				catch
+				{
+					return new Version();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Returns the current version of the app, as defined in the PList, e.g. "4.3".
@@ -238,7 +246,7 @@ namespace Plugin.DeviceInfo
 #if __IOS__
 		public Platform Platform => Platform.iOS;
 #elif __MACOS__
-        public Abstractions.Platform Platform => Abstractions.Platform.macOS;
+		public Abstractions.Platform Platform => Abstractions.Platform.macOS;
 #elif __WATCHOS__
         public Platform Platform => Platform.watchOS;
 #elif __TVOS__
@@ -246,15 +254,15 @@ namespace Plugin.DeviceInfo
 #endif
 
 
-        /// <summary>
-        /// Returns the idiom type of the device
-        /// </summary>
-        public Idiom Idiom
-        {
-            get
-            {
+		/// <summary>
+		/// Returns the idiom type of the device
+		/// </summary>
+		public Idiom Idiom
+		{
+			get
+			{
 #if __MACOS__
-                return Idiom.Desktop;
+				return Idiom.Desktop;
 #elif __WATCHOS__
                 return Idiom.Watch;
 #elif __TVOS__
@@ -275,17 +283,17 @@ namespace Plugin.DeviceInfo
                         return Idiom.Unknown;
                 }
 #endif
-            }
+			}
 
-        }
+		}
 
-        /// <summary>
-        /// Checks whether this is a real device or an emulator/simulator
-        /// </summary>
+		/// <summary>
+		/// Checks whether this is a real device or an emulator/simulator
+		/// </summary>
 #if __MACOS__
-        public bool IsDevice => true; // There is no simulator for mac OS
+		public bool IsDevice => true; // There is no simulator for mac OS
 #else
         public bool IsDevice => Runtime.Arch == Arch.DEVICE;
 #endif
-    }
+	}
 }
