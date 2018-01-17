6df680b8a4c7f35b0d71294addf98886acbe6d46
tafaly <tayoufabrice@yahoo.fr>
2018-01-17 11:46:51 +0100

    MacOS missing files

---
+++ samples/DeviceInfoSample/DeviceInfoSampleMacOS/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace DeviceInfoSampleMacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate(); // add this line
+            NSApplication.Main(args);
+        }
+    }
+}
